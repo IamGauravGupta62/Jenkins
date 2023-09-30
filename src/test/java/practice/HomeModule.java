9ff5d12b1761fe883e5ecfdd5b5db6e36a8254cc
IamGauravGupta62 <gauravmodanwal62@gmail.com>
2023-09-30 14:33:03 +0530

    1st commit

---
+++ src/test/java/practice/HomeModule.java
@@ -0,0 +1,11 @@
+package practice;
+
+import java.security.DomainCombiner;
+
+public class HomeModule {
+	public static void main(String[] args) {
+		
+		System.out.println("---home module is working---");
+	}
+
+}
---
+++ src/test/java/practice/LoginModule.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class LoginModule {
+	public static void main(String[] args) {
+		 
+		System.out.println("---Login is working---");
+	}
+
+}
