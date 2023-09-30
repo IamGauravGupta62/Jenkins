b5ccf5e3e4ece9ef2b42c37ecc82a0f359efde9f
IamGauravGupta62 <gauravmodanwal62@gmail.com>
2023-09-30 17:17:06 +0530

    2nd commit

---
+++ src/test/java/practice/BrowserTest.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class BrowserTest {
+	public static void main(String[] args) {
+		
+		System.out.println("--Browse for a product--");
+		System.out.println("--add the product---");
+	}
+
+}
